e9b821ebb0d2f685c670eba8e08e84436bdf5a09
Thiefmooner <moonthief@foxamil.com>
2025-01-28 22:49:30 +0800

    feat:初始化router

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue'
 
 import 'normalize.css'
 import './assets/css/reset.less' //在main.ts引入reset.less
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
