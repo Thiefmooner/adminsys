c9b81812d881c7ede841fc44a1caa9ff46bfe0e5
Thiefmooner <moonthief@foxamil.com>
2025-02-02 23:24:24 +0800

    feat:初始化axios,最新的axios有类型问题,安装0.25.0版本

---
+++ src/service/request/index.ts
@@ -0,0 +1,73 @@
+import axios from 'axios'
+import type { AxiosInstance } from 'axios'
+import type { HYRequestConfig } from './type'
+
+class HYRequest {
+  instance: AxiosInstance
+
+  constructor(config: HYRequestConfig) {
+    this.instance = axios.create(config)
+
+    this.instance.interceptors.request.use(
+      (config) => {
+        console.log('全局请求成功的拦截')
+        return config
+      },
+      (error) => {
+        console.log('全局请求失败的拦截')
+        return error
+      }
+    )
+    this.instance.interceptors.response.use(
+      (res) => {
+        console.log('全局响应成功的拦截')
+        return res.data
+      },
+      (error) => {
+        console.log('全局响应失败的拦截')
+        return error
+      }
+    )
+    this.instance.interceptors.request.use(
+      config.interceptors?.requestSuccessFn,
+      config.interceptors?.requestFailureFn
+    )
+    this.instance.interceptors.response.use(
+      config.interceptors?.responseSuccessFn,
+      config.interceptors?.responseFailureFn
+    )
+  }
+  request<T = any>(config: HYRequestConfig<T>) {
+    if (config.interceptors?.requestSuccessFn) {
+      config = config.interceptors.requestSuccessFn(config)
+    }
+    return new Promise<T>((resolve, reject) => {
+      this.instance
+        .request<any, T>(config)
+        .then((res) => {
+          if (config.interceptors?.responseSuccessFn) {
+            res = config.interceptors.responseSuccessFn(res)
+          }
+          resolve(res)
+        })
+        .catch((err) => {
+          reject(err)
+        })
+    })
+  }
+
+  get<T = any>(config: HYRequestConfig<T>) {
+    return this.request({ ...config, method: 'GET' })
+  }
+  post<T = any>(config: HYRequestConfig<T>) {
+    return this.request({ ...config, method: 'POST' })
+  }
+  delete<T = any>(config: HYRequestConfig<T>) {
+    return this.request({ ...config, method: 'DELETE' })
+  }
+  patch<T = any>(config: HYRequestConfig<T>) {
+    return this.request({ ...config, method: 'PATCH' })
+  }
+}
+
+export default HYRequest
--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,18 +1,8 @@
 <template>
-  <div class="main">
-    这是mainVue文件,这是counter的值{{ counterStore.counter }}
-  </div>
-  <button @click="changeNumFunc">点击改变数字</button>
+  <div class="main">这是mainVue文件</div>
 </template>
 
-<script setup lang="ts">
-import useCounterStore from '@/store/counter.ts'
-const counterStore = useCounterStore()
-
-const changeNumFunc = () => {
-  counterStore.changeCounterAction(64)
-}
-</script>
+<script setup lang="ts"></script>
 
 <style lang="less" scoped>
 .main {
