d840b7479bed9e0a1ba9deacf341778ec923c526
Thiefmooner <moonthief@foxamil.com>
2025-02-02 17:51:15 +0800

    feat:初始化pinia

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import App from './App.vue'
 import 'normalize.css'
 import './assets/css/reset.less' //在main.ts引入reset.less
 import router from './router'
+import pinia from '@/store'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app')
