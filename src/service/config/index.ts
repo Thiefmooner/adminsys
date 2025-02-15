26ec1db781445f6cd575e130fdb165c9fd07a941
Thiefmooner <moonthief@foxamil.com>
2025-02-15 23:46:47 +0800

    feat:区分生产开发

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,2 +1,10 @@
-export const BASE_URL = 'http://codercba.com:8000'
+let BASE_URL = ''
+//vite提供import.meta.env.MODE，区分开发生产
+if (import.meta.env.MODE === 'production') {
+  BASE_URL = 'http://codercba.prod:8000'
+} else {
+  BASE_URL = 'http://coderwhy.dev:8000'
+}
+console.log(BASE_URL)
+export { BASE_URL }
 export const TIME_OUT = 10000
