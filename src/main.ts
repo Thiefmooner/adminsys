9603ebf2535927f8c51963595f4b2ca505ccc7fb
Thiefmooner <moonthief@foxamil.com>
2025-02-16 20:28:11 +0800

    feat:全局引入elementplus,并测试

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,7 @@ import './assets/css/reset.less' //在main.ts引入reset.less
 import router from './router'
 import pinia from '@/store'
 
-createApp(App).use(router).use(pinia).mount('#app')
+import ElementPlus from 'element-plus' //step1
+import 'element-plus/dist/index.css' //step2
+//全局注册很简单明了
+createApp(App).use(router).use(pinia).use(ElementPlus).mount('#app')
