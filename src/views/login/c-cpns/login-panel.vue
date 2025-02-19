928fc34346bfa8d1a0df1d2cd11b0c6a9a30f78c
Thiefmooner <moonthief@foxmail.com>
2025-02-20 01:05:07 +0800

    feat:去除package.json里面的eslint还是不行，将webstorm里面的setting禁了就可以，改了些登录的样式

---
+++ src/views/login/c-cpns/login-panel.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="login-panel">
+    <h1 class="title">后台管理系统</h1>
+    <div class="tabs">tabs</div>
+    <div class="control-account">
+      <el-checkbox v-model="isRemember" label="记住密码" size="large" />
+      <el-link type="primary">忘记密码</el-link>
+    </div>
+    <el-button class="login-btn" type="primary" size="large"
+      >立即登录</el-button
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const isRemember = ref(false)
+</script>
+
+<style lang="less" scoped>
+.login-panel {
+  width: 330px;
+  margin-bottom: 150px;
+  .title {
+    text-align: center;
+    margin-bottom: 15px;
+  }
+  .control-account {
+    margin-top: 12px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .login-btn {
+    margin-top: 10px;
+    width: 100%;
+    //--el-button-size: 50px;
+  }
+}
+</style>
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="login">这是loginVue文件</div>
+  <div class="login">
+    <login-panel></login-panel>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LoginPanel from '../login/c-cpns/login-panel.vue'
+</script>
 
 <style lang="less" scoped>
 .login {
@@ -10,7 +14,7 @@
   align-items: center;
   justify-content: center;
 
-  width: 100%; //为什么这里宽高可以使用100%了?因为父级APP已经占满了
+  width: 100%; //为什么这里宽高可以使用100%了?因为父级app已经占满了
   height: 100%;
   background: url('@/assets/image/login-bg.svg'); //改成@开头就好了，..开头提示有错误
 }
