dd50a341f5e93487db6c852c0d52c86580d85ae0
Thiefmooner <moonthief@foxmail.com>
2025-02-26 01:38:29 +0800

    feat:安装图标，全局注册下

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import 'normalize.css'
 import './assets/css/reset.less' //在main.ts引入reset.less
 import router from './router'
 import pinia from '@/store'
+import registerIcons from '@/global/register-icons.ts'
 
-createApp(App).use(router).use(pinia).mount('#app')
+createApp(App).use(router).use(registerIcons).use(pinia).mount('#app')
