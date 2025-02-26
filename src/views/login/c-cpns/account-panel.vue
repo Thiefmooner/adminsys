36ab7ed1e64fc4476a19a5517c9cca2e59e0bf57
Thiefmooner <moonthief@foxmail.com>
2025-02-26 21:00:13 +0800

    feat:账号密码校验，非常重要

--- src/views/login/c-cpns/account-panel.vue
+++ src/views/login/c-cpns/account-panel.vue
@@ -1,16 +1,36 @@
-<script setup lang="ts"></script>
-
 <template>
   <div class="panel">
-    <el-form label-width="60px">
-      <el-form-item label="账号">
-        <el-input />
+    <!-- :model="" el-form整个表单绑定account对象-->
+    <el-form 定:model="account" label-width="60px" :rules="rules">
+      <!--这个prop非常重要，他标识着，el-form绑的rules到了账号这里要寻找name的规则-->
+      <el-form-item label="账号" prop="name">
+        <el-input v-model="account.name" />
       </el-form-item>
-      <el-form-item label="密码">
-        <el-input />
+      <el-form-item label="密码" prop="pwd">
+        <el-input v-model="account.pwd" show-password />
       </el-form-item>
     </el-form>
   </div>
 </template>
+<script setup lang="ts">
+import { reactive } from 'vue'
+import type { FormRules } from 'element-plus'
+const account = reactive({
+  name: '',
+  pwd: ''
+})
+//定义校验规则
+const rules: FormRules = reactive({
+  name: [
+    { required: true, message: '必须输入', trigger: 'blur' },
+    {
+      pattern: /^[a-z0-9]{6,20}$/,
+      message: '长度要在6-20,且由字母和数字组成',
+      trigger: 'change'
+    }
+  ],
+  pwd: [{ required: true, message: '必须输入密码', trigger: 'blur' }]
+})
+</script>
 
 <style scoped lang="less"></style>
