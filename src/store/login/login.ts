9954fd71f67d17de72fb9da104909bff6c84a850
Thiefmooner <moonthief@foxmail.com>
2025-02-28 22:36:17 +0800

    fixed:localStorage本地缓存token

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -4,16 +4,19 @@ import type { IAccount } from '@/store/type'
 const useLoginStore = defineStore('login', {
   state: () => ({
     id: '',
-    token: '',
+    token: localStorage.getItem('token') ?? '',
     name: ''
   }),
   actions: {
     async loginAccountAction(account: IAccount) {
       const loginResult = await accountLoginRequest(account)
-      //console.log(loginResult)
+      //1.登录获取token等信息
       this.id = loginResult.data.id
       this.token = loginResult.data.token
       this.name = loginResult.data.name
+
+      //2.localStorage本地缓存token
+      localStorage.setItem('token', this.token)
     }
   }
 })
