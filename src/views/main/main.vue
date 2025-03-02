df6df97d1ceadba9313f5a80156327966e706238
Thiefmooner <moonthief@foxmail.com>
2025-03-03 00:24:10 +0800

    feat:退登

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="main">这是mainVue文件</div>
+  <button @click="quitLogin">退出登录</button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { localCache } from '@/utils/cache.ts'
+import { LOGIN_TOKEN } from '@/global/global_variables'
+import router from '@/router'
+function quitLogin() {
+  localCache.removeCache(LOGIN_TOKEN)
+  router.push('/login')
+}
+</script>
 
 <style lang="less" scoped>
 .main {
